bf468915719dddf51315bfa726c20c10c3d97b31
JihunSKKU <112751329+JihunSKKU@users.noreply.github.com>
2023-11-13 17:22:20 +0900

    Feat2 (#7)
    * Code refactoring
    
    * Code edit

--- M1_test.py
+++ M1_test.py
@@ -51,64 +51,41 @@ csps_fc_biases.append(model_cnn['FC2.bias'])
 strides = [3]
 paddings = [0]
 
-# print()
-# print("Conv1.weight:\t", csps_conv_weights[0].shape)
-# print("Conv1.bias:\t", csps_conv_biases[0].shape)
-# print("FC1.weight:\t", csps_fc_weights[0].shape)
-# print("FC1.bias:\t", csps_fc_biases[0].shape)
-# print("FC2.weight:\t", csps_fc_weights[1].shape)
-# print("FC2.bias:\t", csps_fc_biases[1].shape)
-# print()
-
 def enc_test(evaluator, ckks_encoder, galois_key, relin_keys, csps_ctxt, csps_conv_weights, csps_conv_biases, image_size, paddings, strides, data_size, label, scale):
-    global labels
-    global depth_time
-    global convs
-    global sqs
-    # global avgpools
-    global flattens
-    global fcs
-    global totals
-    global errors
-    global originals
-    global hes
-    # global origin_values
-    # global he_values
-    # global error_values
-    global real_labels
-
     START_TIME = time.time()
 
     result = re_depth(ckks_encoder, evaluator, relin_keys, [csps_ctxt], 4)
     DEPTH_TIME = time.time()
-    print('DROP DEPTH TIME', DEPTH_TIME - START_TIME)
+
+    print('DROP DEPTH TIME\t%.3f' %(DEPTH_TIME - START_TIME))
 
     result, OH, S, const_param = conv2d_layer_converter_(evaluator, ckks_encoder, galois_key, relin_keys, result, csps_conv_weights[0], csps_conv_biases[0], input_size=image_size, real_input_size=image_size, padding=paddings[0], stride=strides[0], data_size=data_size, const_param=1)
     CHECK_TIME1 = time.time()
-    print('CONV2D 1 TIME', CHECK_TIME1-START_TIME)
+    print('CONV2D 1 TIME\t%.3f' %(CHECK_TIME1-START_TIME))
 
     result, const_param = square(evaluator, relin_keys, result, const_param=const_param)
     CHECK_TIME2 = time.time()
-    print('SQ 1 TIME', CHECK_TIME2-CHECK_TIME1)
+    print('SQ 1 TIME\t%.3f' %(CHECK_TIME2-CHECK_TIME1))
 
     result = flatten(evaluator, ckks_encoder, galois_key, relin_keys, result, OH, OH, S, input_size=image_size, data_size=data_size, const_param=const_param)
     CHECK_TIME3 = time.time()
-    print('FLATTEN TIME', CHECK_TIME3-CHECK_TIME2)
+    print('FLATTEN TIME\t%.3f' %(CHECK_TIME3-CHECK_TIME2))
 
     result = fc_layer_converter(evaluator, ckks_encoder, galois_key, relin_keys, result, csps_fc_weights[0], csps_fc_biases[0], data_size=data_size)
     CHECK_TIME4 = time.time()
-    print('FC1 TIME', CHECK_TIME4-CHECK_TIME3)
+    print('FC1 TIME\t%.3f' %(CHECK_TIME4-CHECK_TIME3))
 
     result, const_param = square(evaluator, relin_keys, result, const_param=1)
     CHECK_TIME5 = time.time()
-    print('SQ 2 TIME', CHECK_TIME5-CHECK_TIME4)
+    print('SQ 2 TIME\t%.3f' %(CHECK_TIME5-CHECK_TIME4))
 
     result = fc_layer_converter(evaluator, ckks_encoder, galois_key,relin_keys, result, csps_fc_weights[1], csps_fc_biases[1], data_size=data_size)
     END_TIME = time.time()
-    print('FC2 TIME', END_TIME-CHECK_TIME5)
+    print('FC2 TIME\t%.3f' %(END_TIME-CHECK_TIME5))
 
 
-    print('Total Time', END_TIME-START_TIME)
+    print('Total Time\t%.3f sec' %(END_TIME-START_TIME))
+    print()
     
     for i in range(num_of_data):
         max_data_idx = -1
@@ -134,65 +111,51 @@ def enc_test(evaluator, ckks_encoder, galois_key, relin_keys, csps_ctxt, csps_co
         print("real label     |", label[i])
         print("="*30)
 
-
-
 parms = EncryptionParameters(scheme_type.ckks)
 poly_modulus_degree = 8192*2
 parms.set_poly_modulus_degree(poly_modulus_degree)
 bits_scale1 = 40
-# bits_scale2 = 32
-for bits_scale2 in [32]:
-    depth = 11
-    coeff_mod_bit_sizes = [bits_scale1] + [bits_scale2]*depth + [bits_scale1]
-    parms.set_coeff_modulus(CoeffModulus.Create(poly_modulus_degree, coeff_mod_bit_sizes))
+bits_scale2 = 32
+depth = 11
+coeff_mod_bit_sizes = [bits_scale1] + [bits_scale2]*depth + [bits_scale1]
+parms.set_coeff_modulus(CoeffModulus.Create(poly_modulus_degree, coeff_mod_bit_sizes))
 
-    scale = 2.0**bits_scale2
-    context = SEALContext(parms)
-    ckks_encoder = CKKSEncoder(context)
-    slot_count = ckks_encoder.slot_count()
-    
-    keygen = KeyGenerator(context)
-    public_key = keygen.create_public_key()
-    secret_key = keygen.secret_key()
-    galois_key = keygen.create_galois_keys()
-    relin_keys = keygen.create_relin_keys()
-    encryptor = Encryptor(context, public_key)
-    evaluator = Evaluator(context)
-    decryptor = Decryptor(context, secret_key)
+scale = 2.0**bits_scale2
+context = SEALContext(parms)
+ckks_encoder = CKKSEncoder(context)
+slot_count = ckks_encoder.slot_count()
 
-    image_size = 28 # Suppose that image shape is sqaure
-    data_size = calculate_data_size(image_size, csps_conv_weights, csps_fc_weights, strides, paddings)
-    num_of_data = int(slot_count//data_size)
+keygen = KeyGenerator(context)
+public_key = keygen.create_public_key()
+secret_key = keygen.secret_key()
+galois_key = keygen.create_galois_keys()
+relin_keys = keygen.create_relin_keys()
+encryptor = Encryptor(context, public_key)
+evaluator = Evaluator(context)
+decryptor = Decryptor(context, secret_key)
 
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,))
-    ])
-    test_dataset = datasets.MNIST(root='./Data', train=False, transform=transform, download=True)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=num_of_data, shuffle=True, drop_last=True)
+image_size = 28 # Suppose that image shape is sqaure
+data_size = calculate_data_size(image_size, csps_conv_weights, csps_fc_weights, strides, paddings)
+num_of_data = int(slot_count//data_size)
 
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,))
-    ])
-    test_dataset = datasets.MNIST(root='./Data', train=False, transform=transform, download=True)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=num_of_data, shuffle=True, drop_last=True)
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+test_dataset = datasets.MNIST(root='./Data', train=False, transform=transform, download=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=num_of_data, shuffle=True, drop_last=True)
 
-    for index in range(5):
-        data, _label = next(iter(test_loader))
-        data, _label = np.array(data), _label.tolist()
+data, _label = next(iter(test_loader))
+data, _label = np.array(data), _label.tolist()
 
-        new_data = []
-        for i in range(num_of_data):
-            new_data.extend(data[i].flatten())
-            new_data.extend([0] * (data_size - image_size**2))
-        data = torch.Tensor(data)
-        new_data = torch.Tensor(new_data)
+new_data = []
+for i in range(num_of_data):
+    new_data.extend(data[i].flatten())
+    new_data.extend([0] * (data_size - image_size**2))
+data = torch.Tensor(data)
+new_data = torch.Tensor(new_data)
 
-        ctxt = encryptor.encrypt(ckks_encoder.encode(new_data, scale))
-        # ctxt.save('ctxt/mnist_ctxt')
+ctxt = encryptor.encrypt(ckks_encoder.encode(new_data, scale))
+# ctxt.save('ctxt/mnist_ctxt')
 
-        print('result', index+1)
-        enc_test(evaluator, ckks_encoder, galois_key, relin_keys, ctxt, csps_conv_weights, csps_conv_biases, image_size, paddings, strides, data_size, _label, scale)
-        print()
-        
+enc_test(evaluator, ckks_encoder, galois_key, relin_keys, ctxt, csps_conv_weights, csps_conv_biases, image_size, paddings, strides, data_size, _label, scale)
