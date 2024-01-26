72170f71cf150afe8364fc78d45a2c242f082e90
Jihun <112751329+JihunSKKU@users.noreply.github.com>
2024-01-26 17:11:49 +0900

    Add conv1d converter & ECG dataset training and M7 test code (#28)
    * Add conv1d converter & ECG dataset training and M7 test code
    
    * code 수정

--- models/model_structures.py
+++ models/model_structures.py
@@ -169,3 +169,24 @@ class M6(torch.nn.Module):
         out = self.Square2(out)
         out = self.FC2(out)
         return out
+    
+class M7(torch.nn.Module):
+    def __init__(self):
+        super(M7, self).__init__()
+        self.Conv1 = torch.nn.Conv1d(in_channels=1, out_channels=2, kernel_size=2, stride=2, padding=0)
+        self.Square1 = Square()        
+        self.Conv2 = torch.nn.Conv1d(in_channels=2, out_channels=4, kernel_size=2, stride=2, padding=0)
+        self.Flatten = Flatten()
+        self.FC1 = torch.nn.Linear(128, 32)
+        self.Square2 = Square()
+        self.FC2 = torch.nn.Linear(32, 5)
+        
+    def forward(self, x):
+        out = self.Conv1(x)
+        out = self.Square1(out)
+        out = self.Conv2(out)
+        out = self.Flatten(out)
+        out = self.FC1(out)
+        out = self.Square2(out)
+        out = self.FC2(out)
+        return out
